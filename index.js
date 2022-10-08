c96628946ca00ac1ebdb47116ed8c623e8555ad5
Eugene Shevko <evgeniy.shevko@gearheart.io>
2022-10-08 17:36:00 -0600

    Some JS logic added

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
+const blockInput = document.querySelector(".input-block_input")
+const submitBtn = document.querySelector(".submit-btn")
+const printResult = document.querySelector(".result")
+
 function secondsToDhms(seconds) {
-    seconds = Number(seconds);
-    
-    const y = Math.floor(seconds / 31536025.92);
-    const month = Math.floor(seconds % 31536025.92 / 2628001.152);
-    const w = Math.floor(seconds % 2628001.152 / 604800);
-    const d = Math.floor(seconds % 604800 / 86400);
-    const h = Math.floor(seconds % 86400 / 3600);
-    const m = Math.floor(seconds % 3600 / 60);
-    const s = Math.floor(seconds % 60);
-    
-    let yDisplay = y > 0 ? y + (y == 1 ? " year, " : " years, ") : "";
-    let monthDisplay = month > 0 ? month + (month == 1 ? " month, " : " months, ") : "";
-    let wDisplay = w > 0 ? w + (w == 1 ? " week, " : " weeks, ") : "";
-    let dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
-    let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return yDisplay + monthDisplay + wDisplay + dDisplay + hDisplay + mDisplay + sDisplay;
-    }
-    
-    console.log(secondsToDhms(21448911));
+  seconds = Number(seconds)
+
+  const y = Math.floor(seconds / 31536025.92)
+  const month = Math.floor((seconds % 31536025.92) / 2628001.152)
+  const w = Math.floor((seconds % 2628001.152) / 604800)
+  const d = Math.floor((seconds % 604800) / 86400)
+  const h = Math.floor((seconds % 86400) / 3600)
+  const m = Math.floor((seconds % 3600) / 60)
+  const s = Math.floor(seconds % 60)
+
+  let yDisplay = y > 0 ? y + (y == 1 ? " year, " : " years, ") : ""
+  let monthDisplay =
+    month > 0 ? month + (month == 1 ? " month, " : " months, ") : ""
+  let wDisplay = w > 0 ? w + (w == 1 ? " week, " : " weeks, ") : ""
+  let dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : ""
+  let hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : ""
+  let mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : ""
+  let sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : ""
+  return (
+    yDisplay +
+    monthDisplay +
+    wDisplay +
+    dDisplay +
+    hDisplay +
+    mDisplay +
+    sDisplay
+  )
+}
+
+function success() {
+  if (blockInput.value === "") {
+    submitBtn.disabled = true
+  } else {
+    submitBtn.disabled = false
+  }
+}
+
+submitBtn.addEventListener(
+  "click",
+  () => (printResult.innerHTML = secondsToDhms(blockInput.value))
+)
+
+// console.log(inputValue)
+// console.log(secondsToDhms(21448911))
